d16ba1b456c3072b497946835c4ee6cd204a3921
Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>
2021-04-13 21:15:35 +0430

    Add event and option admin panel
    Signed-off-by: Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Event, Option
+
+
+class OptionInline(admin.TabularInline):
+    model = Option
+    readonly_fields = ("id", "created_at")
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    readonly_fields = ("id", "created_at", "updated_at")
+    inlines = (OptionInline,)
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    readonly_fields,
+                    "owner",
+                    "title",
+                    "description",
+                    "timezone",
+                    "optional",
+                ),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "owner",
+                    "title",
+                    "description",
+                    "timezone",
+                    "optional",
+                ),
+            },
+        ),
+    )
+    list_display = ("title", "owner", "timezone", "optional")
+    ordering = ("title", "owner")
+    search_fields = ("title", "description")
