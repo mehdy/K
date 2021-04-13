ad19ef7206db828c313de4bdad4f4c3802a4fef3
Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>
2021-04-13 21:18:02 +0430

    Add ReadOnly and IsOwner permission classes
    Signed-off-by: Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>

--- K/permissions.py
+++ K/permissions.py
@@ -1,4 +1,13 @@
-from rest_framework.permissions import BasePermission
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class ReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)
+
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
 
 
 class UnauthenticatedPost(BasePermission):
@@ -7,3 +16,10 @@ class UnauthenticatedPost(BasePermission):
 
     def has_permission(self, request, view):
         return request.method == "POST"
+
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if isinstance(obj, get_user_model()):
+            return request.user.pk == obj.pk
+        return request.user.pk == obj.owner.pk
